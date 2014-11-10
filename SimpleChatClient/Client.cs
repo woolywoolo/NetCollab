1f1c395b59ea31f729a1325c290b9ade903707c6
x0rdef <dense.mailxr@gmail.com>
2014-11-11 00:34:41 +0100

    Added projects and basic server/client send/receive.

---
+++ SimpleChatClient/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace NetCollab
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress adress = IPAddress.Parse("127.0.0.1");
+
+                TcpClient client = new TcpClient();
+                Console.WriteLine("Connecting to: " + adress.ToString());
+
+                client.Connect("127.0.0.1", 8001);
+
+                Console.WriteLine("[!] Connected to server.");
+                Console.Write("\nEnter message: ");
+
+                String str = Console.ReadLine();
+                Stream stm = client.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting...");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                Console.Write("Server sent: ");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                client.Close();
+                Console.ReadLine();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ SimpleChatServer/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleChatServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress adress = IPAddress.Parse("127.0.0.1"); // local host for now
+
+                TcpListener listener = new TcpListener(adress, 8001);
+                listener.Start();
+
+                Console.WriteLine("Server running @ " + listener.LocalEndpoint);
+                Console.WriteLine("Waiting for connection...");
+
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("\n[!] Connection accepted from " + socket.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                int k = socket.Receive(b);
+                Console.WriteLine("[!] Received message: ");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(b[i]));
+                Console.WriteLine("\n[!] End of message!");
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                socket.Send(asen.GetBytes("Message received!"));
+                Console.WriteLine("\n[!] Sent: \"Message received\" to client.");
+
+                socket.Close();
+                listener.Stop();
+
+                Console.ReadLine();
+            }catch(Exception e){
+                Console.WriteLine("Error: " + e.StackTrace);
+            }
+        }
+    }
+}
